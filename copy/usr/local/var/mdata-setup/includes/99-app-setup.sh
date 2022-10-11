1a4b2e7b693f74c5b9aca53796b1d03aa08ba64c
jfqd <jfqd@blun.org>
2022-10-11 16:06:12 +0200

    fix mailtrain layout

--- copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+HOSTNAME=$(/native/usr/sbin/mdata-get mailtrain_host)
 MYSQL_HOST=$(/native/usr/sbin/mdata-get mysql_host)
 MYSQL_USER=$(/native/usr/sbin/mdata-get mysql_user)
 MYSQL_DB=$(/native/usr/sbin/mdata-get mysql_db)
@@ -27,18 +28,25 @@ function reinstallAllModules {
 
 echo "* Setup installation configuration"
 cd /var/www/mailtrain
+
+IFS=. read -r sub domain tld <<< "${HOSTNAME}"
+
 cat > server/config/production.yaml <<EOT
 user: mailtrain
 group: mailtrain
 roUser: nobody
 roGroup: nobody
 
+defaultLanguage: de-DE
+
 www:
   host: 0.0.0.0
   secret: "${SECRET}"
-  trustedUrlBase: http://localhost:3000
-  sandboxUrlBase: http://localhost:3003
-  publicUrlBase: http://localhost:3004
+  trustedUrlBase: https://${sub}-adm.${domain}.${tld}
+  sandboxUrlBase: https://${sub}-sbx.${domain}.${tld}
+  publicUrlBase: https://${HOSTNAME}
+  proxy: true
+  postSize: 5MB
 
 mysql:
   host: "${MYSQL_HOST}"
@@ -53,16 +61,19 @@ log:
   level: info
 
 builtinZoneMTA:
+  enabled: false
   log:
     level: warn
 
 queue:
-  processes: 5
+  processes: 2
 EOT
     
 cat > server/services/workers/reports/config/production.yaml <<EOT
 log:
   level: warn
+  
+defaultLanguage: de-DE
 
 mysql:
   host: "${MYSQL_HOST}"
@@ -71,16 +82,34 @@ mysql:
   password: "${MYSQL_PWD}"
 EOT
 
+echo "* Import basic sql cause mailtrain has issues with its own sql-files (on mysql 8)"
+mysql -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PWD}" < /usr/local/var/tmp/mailtrain.sql
+
 echo "* Fix git usage"
 git config --global url."https://".insteadOf git://
 git config url."https://".insteadOf git://
 
-echo "* Install node modules"
+echo "* Fix mailtrain layout"
+sed -i \
+    -e "s|<a href=\"https://mailtrain.org\">Mailtrain.org</a>, <a href=\"mailto:info@mailtrain.org\">info@mailtrain.org</a>. <a href=\"https://github.com/Mailtrain-org/mailtrain\">{t('sourceOnGitHub')}</a>|Mailtrain.org, <a href=\"https://qutic.com/de/loesungen/mailtrain-hosting/\">Mailtrain-Hosting</a>|g" \
+    client/src/lib/page.js
+
+sed -i \
+    -e "s*@import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,400i,700,700i|Ubuntu:300,300i,400,400i,700,700i&subset=latin-ext');*@font-face {font-family: 'Ubuntu';font-style: normal;font-weight: 400;src: url('https://qutic.com/fonts/ubuntu/400/ubuntu.eot'); src: local('Ubuntu Regular'), local('Ubuntu-Regular'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.woff') format('woff'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.ttf') format('truetype'), url('https://qutic.com/fonts/ubuntu/400/ubuntu.svg#Ubuntu') format('svg');}\n\n@font-face {font-family: 'Ubuntu';font-style: bold;font-weight: 700;src: url('https://qutic.com/fonts/ubuntu/700/ubuntu.eot'); src: local('Ubuntu Bold'), local('Ubuntu-Bold'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.woff') format('woff'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.ttf') format('truetype'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.svg#Ubuntu') format('svg');}\n\n@font-face {font-family: 'Ubuntu Mono';font-style: normal;font-weight: 400;src: url('https://qutic.com/fonts/ubuntu-mono-mono/400/ubuntu-mono.eot'); src: local('Ubuntu Mono Regular'), local('Ubuntu Mono-Regular'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.woff') format('woff'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.ttf') format('truetype'), url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.svg#Ubuntu%20Mono') format('svg');}\n\n@font-face {font-family: 'Ubuntu Mono';font-style: bold;font-weight: 700;src: url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.eot'); src: local('Ubuntu Mono Bold'), local('Ubuntu Mono-Bold'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.woff') format('woff'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.ttf') format('truetype'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.svg#Ubuntu%20Mono') format('svg');}*" \
+    client/src/scss/mailtrain.scss
+
+sed -i \
+    -e "s#<mj-font name=\"Lato\" href=\"https://fonts.googleapis.com/css?family=Lato:400,700,400italic\" />##" \
+    server/views/subscription/layout.mjml.hbs 
+
+echo "* Install node modules and build client"
 reinstallAllModules
 (cd client && npm run build || true)
 
-echo "* Skip conversion"
-mv server/setup/knex/migrations/20200824160149_convert_to_utf8mb4.js server/setup/knex/migrations/20200824160149_convert_to_utf8mb4.js.bak
+echo "* Fix utf8mb4 conversion"
+sed -i \
+    -e "s|tablas[i].table_name|tablas[i].TABLE_NAME|" \
+    server/setup/knex/migrations/20200824160149_convert_to_utf8mb4.js
 
 chown -R mailtrain:mailtrain .
 chmod o-rwx server/config
@@ -96,8 +125,9 @@ systemctl enable nginx
 systemctl start redis-server
 systemctl enable redis-server
 
-systemctl start mongod
-systemctl enable mongod
+# zonemta dependency
+# systemctl start mongod
+# systemctl enable mongod
 
 systemctl start mailtrain
 systemctl enable mailtrain
