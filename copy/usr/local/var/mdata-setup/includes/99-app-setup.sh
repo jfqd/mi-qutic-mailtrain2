f11acad2591d2926b93fe869688553064b91f319
jfqd <jfqd@blun.org>
2022-10-12 11:51:42 +0200

    make scss changes

--- copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
@@ -67,13 +67,115 @@ echo "* Import basic sql cause mailtrain has issues with its own sql-files (on m
 mysql -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PWD}" < /usr/local/var/tmp/mailtrain.sql
 
 echo "* Fix mailtrain layout"
+# HINT: rebuild client (with mailtrain-init) on changes
 sed -i \
-    -e "s|<a href=\"https://mailtrain.org\">Mailtrain.org</a>, <a href=\"mailto:info@mailtrain.org\">info@mailtrain.org</a>. <a href=\"https://github.com/Mailtrain-org/mailtrain\">{t('sourceOnGitHub')}</a>|Mailtrain.org, <a href=\"https://qutic.com/de/loesungen/mailtrain-hosting/\">Mailtrain-Hosting</a>|g" \
+    -e "s|<a href=\"https://mailtrain.org\">Mailtrain.org</a>, <a href=\"mailto:info@mailtrain.org\">info@mailtrain.org</a>. <a href=\"https://github.com/Mailtrain-org/mailtrain\">{t('sourceOnGitHub')}</a>|Mailtrain.org, <a href=\"https://qutic.com/de/loesungen/mailtrain-hosting/\">hosted by qutic.com</a>|g" \
     client/src/lib/page.js
 
 sed -i \
     -e "s*@import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,400i,700,700i|Ubuntu:300,300i,400,400i,700,700i&subset=latin-ext');*@font-face {font-family: 'Ubuntu';font-style: normal;font-weight: 400;src: url('https://qutic.com/fonts/ubuntu/400/ubuntu.eot'); src: local('Ubuntu Regular'), local('Ubuntu-Regular'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.woff') format('woff'),url('https://qutic.com/fonts/ubuntu/400/ubuntu.ttf') format('truetype'), url('https://qutic.com/fonts/ubuntu/400/ubuntu.svg#Ubuntu') format('svg');}\n\n@font-face {font-family: 'Ubuntu';font-style: bold;font-weight: 700;src: url('https://qutic.com/fonts/ubuntu/700/ubuntu.eot'); src: local('Ubuntu Bold'), local('Ubuntu-Bold'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.woff') format('woff'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.ttf') format('truetype'),url('https://qutic.com/fonts/ubuntu/700/ubuntu.svg#Ubuntu') format('svg');}\n\n@font-face {font-family: 'Ubuntu Mono';font-style: normal;font-weight: 400;src: url('https://qutic.com/fonts/ubuntu-mono-mono/400/ubuntu-mono.eot'); src: local('Ubuntu Mono Regular'), local('Ubuntu Mono-Regular'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.woff') format('woff'),url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.ttf') format('truetype'), url('https://qutic.com/fonts/ubuntu-mono/400/ubuntu-mono.svg#Ubuntu%20Mono') format('svg');}\n\n@font-face {font-family: 'Ubuntu Mono';font-style: bold;font-weight: 700;src: url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.eot'); src: local('Ubuntu Mono Bold'), local('Ubuntu Mono-Bold'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.eot?#iefix') format('embedded-opentype'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.woff2') format('woff2'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.woff') format('woff'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.ttf') format('truetype'),url('https://qutic.com/fonts/ubuntu-mono/700/ubuntu-mono.svg#Ubuntu%20Mono') format('svg');}*" \
     client/src/scss/mailtrain.scss
+
+cat >> client/src/scss/mailtrain.scss << EOF
+body {
+  font-size: 100%
+}
+
+.bg-dark {
+  background-color: #525c66 !important;
+}
+
+.navbar-dark .navbar-nav .nav-link {
+  color: white;
+}
+
+body.mailtrain .navbar-dark .navbar-nav .active > .nav-link:hover {
+  font-weight: bold;
+  text-decoration: underline
+}
+
+.navbar-dark .navbar-nav .show > .nav-link,
+.navbar-dark .navbar-nav .active > .nav-link,
+.navbar-dark .navbar-nav .nav-link.show, 
+.navbar-dark .navbar-nav .nav-link.active {
+  font-weight: bold;
+  text-decoration: underline
+}
+
+breadcrumb-item.active {
+  color: #444;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #525c66;
+  border-color: #525c66;
+}
+
+.btn-primary:hover {
+  color: #fff;
+  background-color: #292e33;
+  border-color: #292e33;
+}
+
+link.active, 
+.nav-pills .show > .nav-link {
+  background-color: #525c66;
+}
+
+nav-pills .nav-link.active, 
+.nav-pills .show > .nav-link {
+  background-color: #525c66;
+}
+
+a {
+  color: #dd4814;
+  text-decoration: none;
+}
+
+a:hover, a:focus {
+  color: #97310e;
+  text-decoration: underline;
+}
+
+code {
+  color: #c7254e;
+}
+
+footer.app-footer {
+  margin-top: 2rem;
+}
+
+@media (max-width: 1023px) {
+  body.mailtrain .main .container-fluid {
+    padding: 0 10px;
+  }
+
+  body.mailtrain .app-header .navbar {
+    padding: 0 10px;
+  }
+
+  body.mailtrain .mt-breadcrumb-and-tertiary-navbar .breadcrumb {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
+
+@media (min-width: 1024px) {
+  body.mailtrain .main .container-fluid {
+    padding: 0 25px;
+  }
+
+  body.mailtrain .app-header .navbar {
+    padding: 0 25px;
+  }
+
+  body.mailtrain .mt-breadcrumb-and-tertiary-navbar .breadcrumb {
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+}
+EOF
 
 sed -i \
     -e "s#<mj-font name=\"Lato\" href=\"https://fonts.googleapis.com/css?family=Lato:400,700,400italic\" />##" \
